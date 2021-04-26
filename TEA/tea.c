78ab2afde6aaef1440d3f9ed651fe27b6200e7dc
chenxiaoquan233 <chenxiaoquan233@gmail.com>
2021-04-26 08:41:08 +0800

    Update tea.c

--- TEA/tea.c
+++ TEA/tea.c
@@ -2,21 +2,23 @@ const int delta = 0x9E3779B9;
 
 void encrypt(int* value, int* key, int round)
 {
-    int l = value[0], r = value[1];
-    for(int i = 1; i <= round; ++i)
+    int l = value[0], r = value[1], sum = 0;
+    for(int i = 0; i < round; ++i)
     {
-        l += ((r << 4) + key[0]) ^ (r + delta * i) ^ ((r >> 5) + key[1]);
-        r += ((l << 4) + key[2]) ^ (l + delta * i) ^ ((l >> 5) + key[3]);
+        l += ((r << 4) + key[0]) ^ (r + sum) ^ ((r >> 5) + key[1]);
+        sum += delta;
+        r += ((l << 4) + key[2]) ^ (l + sum) ^ ((l >> 5) + key[3]);
     }
     value[0] = l, value[1] = r;
 }
 
 void decrypt(int* value, int* key, int round)
 {
-    int l = value[0], r = value[1];
-    for(int i = round; i > 0; --i)
+    int l = value[0], r = value[1], sum = delta * round;
+    for(int i = 0; i < round; ++i)
     {
         r -= ((l << 4) + key[2]) ^ (l + delta * i) ^ ((l >> 5) + key[3]);
+        sum -= delta;
         l -= ((r << 4) + key[0]) ^ (r + delta * i) ^ ((r >> 5) + key[1]);
     }
     value[0] = l, value[1] = r;
