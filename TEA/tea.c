34be54b8eea05adbe7bf414d259cef9dde900295
chenxiaoquan233 <chenxiaoquan233@gmail.com>
2021-05-10 11:41:08 +0800

    fix errors

--- TEA/tea.c
+++ TEA/tea.c
@@ -5,8 +5,8 @@ void encrypt(unsigned int* value, unsigned int* key, int round)
     unsigned int l = value[0], r = value[1], sum = 0;
     for(int i = 0; i < round; ++i)
     {
-        l += ((r << 4) + key[0]) ^ (r + sum) ^ ((r >> 5) + key[1]);
         sum += delta;
+        l += ((r << 4) + key[0]) ^ (r + sum) ^ ((r >> 5) + key[1]);
         r += ((l << 4) + key[2]) ^ (l + sum) ^ ((l >> 5) + key[3]);
     }
     value[0] = l, value[1] = r;
@@ -17,9 +17,9 @@ void decrypt(unsigned int* value, unsigned int* key, int round)
     unsigned int l = value[0], r = value[1], sum = delta * round;
     for(int i = 0; i < round; ++i)
     {
-        r -= ((l << 4) + key[2]) ^ (l + delta * i) ^ ((l >> 5) + key[3]);
+        r -= ((l << 4) + key[2]) ^ (l + sum) ^ ((l >> 5) + key[3]);
+        l -= ((r << 4) + key[0]) ^ (r + sum) ^ ((r >> 5) + key[1]);
         sum -= delta;
-        l -= ((r << 4) + key[0]) ^ (r + delta * i) ^ ((r >> 5) + key[1]);
     }
     value[0] = l, value[1] = r;
 }
